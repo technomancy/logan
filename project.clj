481a47d6eb87b0a38d45407596f5c24277bf8261
Phil Hagelberg <technomancy@gmail.com>
2014-06-08 08:38:23 -0700

    deploy repositories

--- project.clj
+++ project.clj
@@ -3,6 +3,7 @@
   :url "https://github.com/technomancy/logan"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :deploy-repositories {"releases" "file:///tmp/repo"}
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [ring/ring-jetty-adapter "1.1.0"]
                  [cheshire "4.0.0"]
