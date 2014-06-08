48828104ce13ce7c4b203faea632d881f9495d3f
Phil Hagelberg <technomancy@gmail.com>
2014-06-08 08:37:41 -0700

    lein-release plugin: preparing 0.1.0 release

--- project.clj
+++ project.clj
@@ -1,8 +1,9 @@
-(defproject logan "0.1.0-SNAPSHOT"
+(defproject logan "0.1.0"
   :description "Distributed analysis of IRC logs"
   :url "https://github.com/technomancy/logan"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :deploy-repositories {"snapshots" "file:///tmp/repo"}
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [ring/ring-jetty-adapter "1.1.0"]
                  [cheshire "4.0.0"]
